12e11d93cbe1432c44178181db956e5920f9d151
L Rakesh Singh <keztmckinnon@gmail.com>
2022-09-24 15:00:19 +0530

    init

--- script.js
+++ script.js
@@ -1,31 +1,22 @@
-var userInput=document.querySelector("#user-input")
-var translate=document.querySelector("#translate")
-var output=document.querySelector("#output")
+var userInput = document.querySelector(".user-input")
+var output = document.querySelector(".output")
+var button = document.querySelector(".btn")
 
-translate.addEventListener("click", clickHandler)
+//button.addEventListener("click", checkChar)
+userInput.addEventListener("change", updateValue)
 
-function clickHandler(){
-    var inputValue=userInput.value;
-    fetch(serverUrl(inputValue))
-    .then(response=>{
-        if(response.status===401){
-            output.innerText="user not found"
-        }else if (response.status===404){
-            output.innerText="you are not log in"
-        }else {
-            output.innerText=`Great! you are good to go`
-        }
-    })}
-    // .catch(errorHandler)
+function updateValue() {
+    output.innerText=userInput.value
+    if (userInput.value.length > 5) {
+        output.style.color = "red";
+        output.innerText = userInput.value
 
+    } else {
+        output.style.color = "green";
+        output.innerText = userInput.value;
 
-function serverUrl(text){
-    var api="https://unitube-server.herokuapp.com/playlists"
-    //var api="https://jsonplaceholder.typicode.com/dummyUsers"
-     //var api="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
-    return api+`?text=${text}`
+    }
 }
 
-// function errorHandler(){
-//     alert("Server down--please try after sometime")
-// }
+
+updateValue()
